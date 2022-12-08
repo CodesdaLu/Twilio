e7844294f2cc822c6c13306662b0ab9ca6451ec4
Luana Araruna <111820291+CodesdaLu@users.noreply.github.com>
2022-12-08 18:08:00 -0300

    Update api.wpp.py

--- api.wpp.py
+++ api.wpp.py
@@ -3,8 +3,8 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = 'AC7ee143bc4d8e0fbec758710e659634d1'
-auth_token = '9b5d781bc3e98b99a5dace5400803639'
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
