e5dee0331c83d543d89873374d3b4efbe7b3d1ab
Luana Araruna <111820291+CodesdaLu@users.noreply.github.com>
2022-12-08 18:09:01 -0300

    Update API_message.py

--- API_message.py
+++ API_message.py
@@ -6,8 +6,8 @@ meu_numero = '+5521994602511'
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = 'AC7ee143bc4d8e0fbec758710e659634d1'
-auth_token = '9b5d781bc3e98b99a5dace5400803639'
+account_sid = 'TWILIO_ACCOUNT_SID'
+auth_token = 'TWILIO_AUTH_TOKEN'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
